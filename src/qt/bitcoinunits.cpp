0cd255bc694011c476f90d40afdc4c28fa9dbcfd
ihor-chaban <igor.chaban.1995@gmail.com>
2018-03-26 15:14:08 +0300

    replaced all instances of BLK with BCC

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BLK");
-    case mBTC: return QString("mBLK");
-    case uBTC: return QString::fromUtf8("μBLK");
+    case BTC: return QString("BCC");
+    case mBTC: return QString("mBCC");
+    case uBTC: return QString::fromUtf8("μBCC");
     default: return QString("???");
     }
 }
